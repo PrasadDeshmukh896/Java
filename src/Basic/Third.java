1983e25d31560cf28d13d01e21f841eedee6e530
Prasad Gajananrao Pachpawar <117139808+PrasadDeshmukh896@users.noreply.github.com>
2023-03-25 11:33:28 +0530

    Third Program

---
+++ src/Basic/Third.java
@@ -0,0 +1,16 @@
+package Basic;
+/*
+5-subject
+make sum
+calculate percentage
+ */
+public class Third {
+    public static void main(String[] args) {
+        double s1=60,s2=80,s3=78,s4=78,s5=90;
+        double sum=s1+s2+s3+s4+s5;
+        double percentage=sum/500*100;
+
+        System.out.println("Sum Is "+sum);
+        System.out.println("Percentage Is "+percentage+"%");
+    }
+}
