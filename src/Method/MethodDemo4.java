f8c8ec7bdaf13b7330db3e878a0c43560d83d93b
Prasad Gajananrao Pachpawar <117139808+PrasadDeshmukh896@users.noreply.github.com>
2023-04-04 16:32:51 +0530

    Area Of Circle

---
+++ src/Method/MethodDemo4.java
@@ -0,0 +1,12 @@
+package Method;
+//calculate area of circle
+public class MethodDemo4 {
+
+    public static void areaOfCircle(double radius){
+        double area =3.14*radius*radius;
+        System.out.println("Area Of Circle Is "+area);
+    }
+    public static void main(String[] args) {
+        areaOfCircle(5);
+    }
+}
