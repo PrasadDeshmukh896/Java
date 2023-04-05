6f68d52b298ab109c7f598bbb35fea1de4faf07d
Prasad Gajananrao Pachpawar <117139808+PrasadDeshmukh896@users.noreply.github.com>
2023-04-05 20:47:06 +0530

    NonVoidBasic

---
+++ src/Method/MethodDemo8.java
@@ -0,0 +1,16 @@
+package Method;
+/*
+Non-Void Basic Demo
+ */
+public class MethodDemo8 {
+
+    static int addition(){
+        int result=20+35;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int output=addition();
+        System.out.println("The Addition Is "+output);
+    }
+}
