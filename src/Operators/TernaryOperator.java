041a65578bd749976fad106e8c7b3454bd116386
Prasad Gajananrao Pachpawar <117139808+PrasadDeshmukh896@users.noreply.github.com>
2023-05-08 20:16:30 +0530

    TernaryOperator

--- src/Operators/TernaryOperator.java
+++ src/Operators/TernaryOperator.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class TernaryOperator {
     public static void main(String[] args) {
         Scanner sc1=new Scanner(System.in);
-        System.out.println("Enter Number");
-        double number =sc1.nextDouble();
+        System.out.println("Enter Number ");
+        double number= sc1.nextDouble();
 
-        String result=(number%2==0)?"Even":"Odd";
+        String result=(number%2==0)?"Even ":"Odd ";
 
-        System.out.println("Given Number Is "+result+" Number");
-
+        System.out.println("Given Number Is "+result+"Number");
     }
 }
